4b948baae2403ccc2ec6dae6d4ac38162b150241
Cecilia Rivero <ceciliarc@yahoo.com>
2014-05-06 13:18:10 -0300

    Fix validation for end_date in new_ballot.rb.

--- filters/new_ballot.rb
+++ filters/new_ballot.rb
@@ -8,10 +8,9 @@ class NewBallot < Scrivener
 
     if assert_present(:end_choices_date)
       assert(end_choices_date > Time.new.to_i, [:end_choices_date, :not_valid])
-    end
-
-    if assert_present(:end_date)
-      assert(end_date > end_choices_date, [:end_date, :not_valid])
+      if assert_present(:end_date)
+        assert(end_date > end_choices_date, [:end_date, :not_valid])
+      end
     end
   end
 end
