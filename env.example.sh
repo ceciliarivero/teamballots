709d2ab7dcb7494c9b674eadf2ddfb9d98fd474a
Cecilia Rivero <contact@ceciliarivero.com>
2014-11-09 11:18:30 -0300

    Add env.example.sh.

---
+++ env.example.sh
@@ -0,0 +1,8 @@
+APP_SECRET=your_app_secret_here
+MALONE_URL=smtp://username:password@smtp.gmail.com:587
+NEW_RELIC_LICENSE_KEY=your_new_relic_license_key
+NEWRELIC_AGENT_ENABLED=true
+NOBI_SECRET=your_nobi_secret_here
+OPENREDIS_URL=redis://127.0.0.1:6379
+RACK_ENV=production
+RESET_URL=http://localhost:9393
